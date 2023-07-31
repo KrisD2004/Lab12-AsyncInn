9384cf74bb5a53c172d0ef6a305838daaa35754b
Kristian Dotson <KDotson387@gmail.com>
2023-07-31 10:52:33 -0500

    added more dummy data

--- Data/AsyncInnContext.cs
+++ Data/AsyncInnContext.cs
@@ -44,6 +44,86 @@ namespace Lab2_AysncInn.Data
             modelBuilder.Entity<HotelRoom>().HasData(new HotelRoom { ID = 1, HotelID = 1, RoomID = 1, Price = 100.78 });
 
 
+            //second dummy data
+            modelBuilder.Entity<Amenity>().HasData(new Amenity
+            {
+                ID = 2,
+                Name = "Free Wi-Fi"
+            });
+
+            modelBuilder.Entity<RoomAmenity>().HasData(new RoomAmenity
+            {
+                ID = 2,
+                AmenityID = 2,
+                RoomID = 2
+            });
+
+            modelBuilder.Entity<Room>().HasData(new Room
+            {
+                ID = 2,
+                Layout = 1,
+                Name = "Deluxe Room"
+            });
+
+            modelBuilder.Entity<HotelRoom>().HasData(new HotelRoom
+            {
+                ID = 2,
+                HotelID = 1,
+                RoomID = 2,
+                Price = 150.99
+            });
+
+            modelBuilder.Entity<Hotel>().HasData(new Hotel
+            {
+                ID = 2,
+                Address = "456 Oak St",
+                City = "New York",
+                State = "NY",
+                Name = "Central Hotel",
+                Phone = "555-999-9999"
+            });
+
+            //third dummy 
+            modelBuilder.Entity<Amenity>().HasData(new Amenity
+            {
+                ID = 3,
+                Name = "Swimming Pool"
+            });
+
+            modelBuilder.Entity<RoomAmenity>().HasData(new RoomAmenity
+            {
+                ID = 3,
+                AmenityID = 3,
+                RoomID = 3
+            });
+
+            modelBuilder.Entity<Room>().HasData(new Room
+            {
+                ID = 3,
+                Layout = 2,
+                Name = "Suite Room"
+            });
+
+            modelBuilder.Entity<HotelRoom>().HasData(new HotelRoom
+            {
+                ID = 3,
+                HotelID = 3,
+                RoomID = 3,
+                Price = 180.00
+            });
+
+
+            modelBuilder.Entity<Hotel>().HasData(new Hotel
+            {
+                ID = 3,
+                Address = "789 Pine St",
+                City = "Los Angeles",
+                State = "CA",
+                Name = "Sunset Hotel",
+                Phone = "555-777-7777"
+            });
+
+
         }
 
         public DbSet<Lab2_AysncInn.Models.Hotel> Hotel { get; set; } = default!;
